6567c2f66fad10d4801d427b665a0a67aaca59ee
Ian Zhang <ianzhang1986@gmail.com>
2016-02-03 22:07:32 -0800

    [Feature] Sync sub folder

--- localServer/photonVoodooComm/led-socket.js
+++ localServer/photonVoodooComm/led-socket.js
@@ -1,7 +1,51 @@
 var Particle = require('particle-io');
+var http = require('http');
+var request = require('request');
+
+var HEROKU_HOST = "https://powerful-ravine-17147.herokuapp.com/";
+var PATH = "decision/";
+var CREDENTIAL = 'open';
 
 var LOW = 0;
 var HIGH = 1;
+/*******************************************************************************
+ * Function Name  :
+ * Description    :
+ * Input          : Pin
+ * Output         : None.
+ * Return         :
+ * Returns
+ ********************************************************************************/
+function checkCredential(target, CREDENTIAL) {
+  return target === CREDENTIAL;
+}
+// JSON body parser
+function parseResponse(res) {
+  console.log("RECEIVED RESPONSE from Remote Server", res.body);
+  return res.body;
+}
+
+/*
+function getAuthFromRemote(action) {
+  request
+    .post(HEROKU_HOST + PATH, {form: {key: 'value'}})
+    .on('response', function(response) {
+      var openDoor = checkCredential(parseResponse(response), CREDENTIAL);
+      action(openDoor);
+    })
+    .on('error', function(err) {
+      throw "Encounter Errors when make get request to " + HEROKU_HOST + PATH;
+    });
+}*/
+
+function getAuthFromRemote(action) {
+  request.post({url: HEROKU_HOST + PATH, form: {key: 'value'}}, function(err, httpResp, body){
+//    console.log(body);
+    var openDoor = checkCredential(body, CREDENTIAL);
+    action(openDoor);
+  });
+}
+
 /*******************************************************************************
  * Function Name  : debounce
  * Description    : Software key debounce
@@ -60,11 +104,16 @@ var smoothFilter = function(newNumber) {
   return sumQueue >= BUF_LEN/2;
 };
 
+/*
 var board = new Particle({
   host: '192.168.1.117',
   port: 48879
 });
-
+*/
+var board = new Particle({
+  host: '10.84.18.7',
+  port: 48879,
+});
 
 board.on('ready', exec_context);
 
@@ -79,8 +128,12 @@ function exec_context() {
   debounce.call(this, 'D6', function(state) {
 //    console.log(state);
     if (D6preState !== smoothFilter(state)) {
-      self.digitalWrite('D7', D7State = negState(D7State));
 //      console.log(D7State);
+      getAuthFromRemote(function(openDoor) {
+        if (openDoor) {
+          self.digitalWrite('D7', D7State = negState(D7State));
+        }
+      });
     }
     preState = state;
   });
