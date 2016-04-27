dddd003f76017422e16231d158b1cf9632b5ce19
Ian Zhang <ianzhang1986@gmail.com>
2016-04-26 23:44:23 -0700

    [Feature] First step in setting up redBearLab uart_loop_back

---
+++ redBearLab/uart_loop_back/uart_loop_back.ino
@@ -0,0 +1,13 @@
+/**
+ * BLE Nano/nRF51822 UART loopback
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
